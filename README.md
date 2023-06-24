946a66471cfce7b48f82ada09ae938da0c82740b
Jay Lee <jayin920805@gmail.com>
2023-06-24 14:43:43 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Batch Event Adder
+
+一個可以批次新增日曆活動的工具
+
+## Usage
+
+1. `pip install -r requirements.txt`
+2. 在 `input.txt` 加入需要新增的活動，格式可以參考 `input.txt`
+3. `python main.py`
+4. `export.ics` 即為最終的輸出檔
---
+++ main.py
@@ -0,0 +1,22 @@
+import ics
+
+c = ics.Calendar()
+
+cur_date = None
+
+with open("input.txt", "r") as f:
+    for line in f:
+        if line.startswith("Date:"):
+            cur_date = line[5:].strip()
+        else:
+            line = line.split(" ")
+            begin = line[0].split("-")[0]
+            end = line[0].split("-")[1]
+            # use taipei timezone
+            begin = begin + "+08:00"
+            end = end + "+08:00"
+            name = " ".join(line[1:]).strip()
+            c.events.add(ics.Event(name=name, begin=cur_date + " " + begin, end=cur_date + " " + end))
+
+with open('export.ics', 'w') as my_file:
+    my_file.writelines(c.serialize_iter())
